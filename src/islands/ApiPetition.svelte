4e259993d1d193a13069cf7fb574c8b43ad1b648
Rafael Carvacho <110637459+Unnunoctio@users.noreply.github.com>
2024-05-20 06:34:21 -0400

    feat: :sparkles: added search form & json viewer

---
+++ src/islands/ApiPetition.svelte
@@ -0,0 +1,19 @@
+<script>
+  export let url;
+
+  const get = async (url) => {
+    const res = await fetch(url)
+    if (!res.ok) return null
+
+    const data = await res.json()
+    return data
+  }
+</script>
+
+{#await get(url)}
+  <span>Loading ...</span>
+{:then json}
+  <pre class="text-sm">
+<code>{JSON.stringify(json, null, 2)}</code>
+  </pre>
+{/await}
---
+++ src/islands/Search.svelte
@@ -0,0 +1,61 @@
+<script lang="ts">
+  import ApiPetition from "./ApiPetition.svelte";
+
+  export let urlPath: string;
+  let urlActive = "brand/corona"
+  let urlPetition = `${urlPath}${urlActive}`
+
+  function submit() {
+    urlPetition = `${urlPath}${urlActive}`
+  }
+
+  function changeInput(value: string) {
+    urlActive = value
+    submit()
+  }
+</script>
+
+<form on:submit|preventDefault={submit} class="flex flex-col sm:flex-row gap-2 sm:gap-0">
+  <section class="flex flex-1 text-[12px] xs:text-base">
+    <label for="url-input" class="p-2 bg-gray-300 rounded-l-lg">{urlPath}</label>
+    <input
+      bind:value={urlActive}
+      id="url-input"
+      name="url-input"
+      type="text"
+      autocomplete="off"
+      autocorrect="off"
+      spellcheck="false"
+      class="flex-1 px-2 min-w-[120px] border border-l-0 sm:border-x-0 border-gray-300 rounded-r-lg sm:rounded-r-none outline-none"
+    />
+  </section>
+  <button type="submit" class="px-8 py-2 font-medium text-secondary bg-blue-500 hover:bg-blue-600 rounded-lg sm:rounded-l-none transition-colors">Enviar</button>
+</form>
+
+<section class="flex flex-wrap mt-2">
+  <p class="pr-1">Otras peticiones:</p>
+  <button on:click={() => changeInput("brand/corona")} class="text-blue-500 hover:underline">
+    brand/corona
+  </button>
+  <p class="pr-1">,</p>
+  <button on:click={() => changeInput("category/cervezas")} class="text-blue-500 hover:underline">
+    category/cervezas
+  </button>
+  <p class="pr-1">,</p>
+  <button on:click={() => changeInput("made-in/alemania")} class="text-blue-500 hover:underline">
+    made-in/alemania
+  </button>
+  <p class="pr-1">, or</p>
+  <button on:click={() => changeInput("drinks?limit=5&page=1")} class="text-blue-500 hover:underline">
+    drinks?limit=5&page=1
+  </button>
+  <p>.</p>
+</section>
+
+<section class="mt-10">
+  <p class="pb-4 truncate">Link directo: <a href={urlPetition} class="text-blue-500 hover:underline">{urlPetition}</a></p>
+  <div class="p-4 h-[600px] overflow-auto bg-slate-700 text-secondary rounded-lg">
+    <ApiPetition url={urlPetition} />
+  </div>
+</section>
+
