19590f81349dccf6f23f4b9f435c33e074542692
Rafael Carvacho <110637459+Unnunoctio@users.noreply.github.com>
2024-05-20 06:29:20 -0400

    chore: :wrench: updated tailwind theme

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      screens: {
+        xs: "480px"
+      },
+			colors: {
+				primary: "#222222",
+        secondary: "#f0f0f0",
+			},
+      maxWidth: {
+        page: "1080px",
+      },
+    },
+  },
+  plugins: [],
+};
