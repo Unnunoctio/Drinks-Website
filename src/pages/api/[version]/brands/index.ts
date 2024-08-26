547c3f2e5d415209536dceb8f99c391dc504277e
Rafael Carvacho <110637459+Unnunoctio@users.noreply.github.com>
2024-08-26 01:14:08 -0400

    feat: :sparkles: add brands route

---
+++ src/pages/api/[version]/brands/index.ts
@@ -0,0 +1,27 @@
+import type { APIRoute } from "astro";
+
+export const GET: APIRoute = async ({ request }) => {
+  const path = request.url.split('/api')[1]
+
+  let url = request.url.split('?')[0]
+  if (url.endsWith('/')) url = url.slice(0, -1)
+
+  const DRINKS_API_URL = import.meta.env.DRINKS_API_URL
+
+  const res = await fetch(DRINKS_API_URL + path)
+  const data = await res.json()
+
+  data.data = data.data.map((item: any) => {
+    return {
+      brand: item.brand,
+      count: item.count,
+      url: url + item.path
+    }
+  })
+
+  return new Response(JSON.stringify(data), {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+}
